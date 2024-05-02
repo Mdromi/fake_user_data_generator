e734c25b87986b4b5574e3fa559a1dce755b8b73
mdromi <mdromi14@gmail.com>
2024-05-03 02:54:55 +0600

    add REDIS_URL production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,10 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.cache_store = :redis_cache_store, {
+    url: ENV["REDIS_URL"],
+  }
+
   # Code is not reloaded between requests.
   config.enable_reloading = false
 
@@ -50,11 +54,11 @@ Rails.application.configure do
 
   # Log to STDOUT by default
   config.logger = ActiveSupport::Logger.new(STDOUT)
-    .tap  { |logger| logger.formatter = ::Logger::Formatter.new }
+    .tap { |logger| logger.formatter = ::Logger::Formatter.new }
     .then { |logger| ActiveSupport::TaggedLogging.new(logger) }
 
   # Prepend all log lines with the following tags.
-  config.log_tags = [ :request_id ]
+  config.log_tags = [:request_id]
 
   # "info" includes generic and useful information about system operation, but avoids logging too much
   # information to avoid inadvertent exposure of personally identifiable information (PII). If you
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,1 @@
-Rails.application.config.session_store :redis_store
+Rails.application.config.session_store :redis_store, servers: ['redis://localhost:6379/0/session']
